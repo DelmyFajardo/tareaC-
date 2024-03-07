19849b6aa35d102b43e463995f9ffa35769f3ab0
delmy <dfajardob@miumg.edu.gt>
2024-03-07 16:39:52 -0600

    cree otra rama
    desde develop

---
+++ conectores-logicos/conectores -logicos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conectores__logicos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool esAdmin = false;
+            bool estaRegistrado = true;
+            string nombreDeUsuarios = "";
+
+        Console.WriteLine("Por favor, ingrese su nombre de usuario");
+        nombreDeUsuarios = Console.ReadLine();
+            if (estaRegistrado && nombreDeUsuarios != "" && nombreDeUsuarios.Equals("Admin"))
+            {
+                Console.WriteLine("Hola usuarios resgistrados");
+                Console.WriteLine("Hola " + nombreDeUsuarios);
+                Console.WriteLine("Hola Admin");
+
+            }
+            if (esAdmin || estaRegistrado)
+            {
+                Console.WriteLine("Estas Loguado");
+            }
+
+        }
+    }
+}
