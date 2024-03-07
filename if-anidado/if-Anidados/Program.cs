1a1013660622ea4066caf373771de9fb43857ac5
delmy <dfajardob@miumg.edu.gt>
2024-03-07 17:11:42 -0600

    desde main2

---
+++ if-anidado/if-Anidados/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_Anidados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool esAdmin = false;
+            bool estaRegistrado = true;
+            string nombreDeUsuario = "";
+
+            Console.WriteLine("por favor, ingrese su nombre de usuario");
+            nombreDeUsuario = Console.ReadLine();
+            if (estaRegistrado)
+            {
+                Console.WriteLine("hola, usuario registrado");
+                if (nombreDeUsuario != "")
+                {
+                    Console.WriteLine("Hola " + nombreDeUsuario);
+                    if (nombreDeUsuario.Equals("Admin"))
+                    {
+                        Console.WriteLine(" hola Admin");
+                    }
+                }
+            }
+        }
+    }
+}
