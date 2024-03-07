95759ab8b48d0b66b0826caec7abcb2c9c7eb0ea
delmy <dfajardob@miumg.edu.gt>
2024-03-07 16:52:23 -0600

    desde fork

---
+++ desiciones/decisiones/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace decisiones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor, ingresa la temperatura actua");
+            string temperatura = Console.ReadLine();
+            int numTemperatura = int.Parse(temperatura);
+            if (numTemperatura < 20)
+            {
+                Console.WriteLine("Abrigate!");
+            }
+            if (numTemperatura == 20)
+            {
+                Console.WriteLine("VIste comodo!");
+            }
+            if (numTemperatura > 20)
+            {
+                Console.WriteLine("Usa ropa liviana");
+            }
+            Console.ReadLine();
+        }
+    }
+}
