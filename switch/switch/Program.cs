c9394564c9e1878cd79ab25e688315783111ff90
delmy <dfajardob@miumg.edu.gt>
2024-03-07 16:54:25 -0600

    otra vez

---
+++ switch/switch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string piedraPapelTijera = "Tijera";
+            switch (piedraPapelTijera)
+            {
+                case "Piedra":
+                    Console.WriteLine("Piedra mata a tijera ");
+                    break;
+                case "Papel":
+                    Console.WriteLine("Papel mata a piedra ");
+                    break;
+                case "Tijera":
+                    Console.WriteLine(" Tijera  mata a papel ");
+                    break;
+                default:
+                    Console.WriteLine(" ese elemento no forma parte del juego  ");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
